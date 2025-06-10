0c18e4c31dd328b31fd389e794f68396c3273a4d
aquapi <aquaplmc@gmail.com>
2025-06-10 22:55:24 +0700

    Update

--- bench/all/src/baseline.case.ts
+++ bench/all/src/baseline.case.ts
@@ -3,21 +3,21 @@ export default defineCase({
   name: 'baseline',
   tests: {
     assertLoose: () => (o) =>
-        o !== null &&
-        typeof o === 'object' &&
-        typeof o.number === 'number' &&
-        typeof o.negNumber === 'number' &&
-        typeof o.maxNumber === 'number' &&
-        typeof o.string === 'string' &&
-        typeof o.longString === 'string' &&
-        typeof o.boolean === 'boolean' &&
-        o.deeplyNested !== null &&
-        typeof o.deeplyNested === 'object' &&
-        typeof o.deeplyNested.foo === 'string' &&
-        typeof o.deeplyNested.num === 'number' &&
-        typeof o.deeplyNested.bool === 'boolean' &&
-        Array.isArray(o.items) &&
-        o.items.every((_: any) => typeof _ === 'number'),
+      o !== null &&
+      typeof o === 'object' &&
+      typeof o.number === 'number' &&
+      typeof o.negNumber === 'number' &&
+      typeof o.maxNumber === 'number' &&
+      typeof o.string === 'string' &&
+      typeof o.longString === 'string' &&
+      typeof o.boolean === 'boolean' &&
+      o.deeplyNested !== null &&
+      typeof o.deeplyNested === 'object' &&
+      typeof o.deeplyNested.foo === 'string' &&
+      typeof o.deeplyNested.num === 'number' &&
+      typeof o.deeplyNested.bool === 'boolean' &&
+      Array.isArray(o.items) &&
+      o.items.every((_: any) => typeof _ === 'number'),
     stringify: () => JSON.stringify,
   },
 });
