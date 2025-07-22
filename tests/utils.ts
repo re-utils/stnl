b78cd3deb9df0b0633b861a4c671eb628b016fcb
aquapi <aquaplmc@gmail.com>
2025-07-22 22:17:14 +0700

    Update

--- tests/utils.ts
+++ tests/utils.ts
@@ -4,7 +4,6 @@ export const schema = <const T extends t.TLoadedType>(
   s: T,
 ): ((o: t.TInfer<T>) => void) => {
   console.log('Assert:', build.json.assert.code(s));
-  console.log('Stringify:', build.json.stringify.code(s));
 
   const assert = build.json.assert.compile(s);
   const stringify = build.json.stringify.compile(s);
